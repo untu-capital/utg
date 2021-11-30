4750c0442377546c70613c07f10ce44997d85d87
Nyasha Chirinda - Dell <nyasha.chirinda@untu-capital.com>
2021-11-30 16:24:39 +0200

    add auth classes

---
+++ src/main/java/com/untucapital/usuite/utg/auth/UserPrincipalService.java
@@ -0,0 +1,42 @@
+package com.untucapital.usuite.utg.auth;
+
+import com.untucapital.usuite.utg.model.User;
+import com.untucapital.usuite.utg.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author Chirinda Nyasha Dell 22/11/2021
+ */
+
+@Service
+@Transactional
+public class UserPrincipalService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserPrincipalService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return UserPrincipal.create(user);
+    }
+
+    // This method is used by the AuthFilter
+    public UserPrincipal loadUserById(String id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with id: " + id));
+
+        return UserPrincipal.create(user);
+    }
+}
