69a393675051eb64cd86cdeb59c0c2aed5055c47
Kelvin Randa <kelvin.randa@untu-capital.com>
2021-12-09 16:23:40 +0200

    email functionality for user cinfirmation

--- src/main/java/com/untucapital/usuite/utg/service/UserService.java
+++ src/main/java/com/untucapital/usuite/utg/service/UserService.java
@@ -78,6 +78,8 @@ public class UserService extends AbstractService<User> {
     public Optional<LoginResp> authenticateUser(LoginReq loginReq) {
         log.debug("User Authentication Request - {}", FormatterUtil.toJson(loginReq));
 
+        validateUserStatus(loginReq.getUsername());
+
         Authentication authentication = authenticationManager
                 .authenticate(new UsernamePasswordAuthenticationToken(loginReq.getUsername(), loginReq.getPassword()));
 
@@ -91,6 +93,30 @@ public class UserService extends AbstractService<User> {
             );
         }
         return loginRespOptional;
+    }
+
+    private void validateUserStatus(String username) throws SecurityException {
+
+        String failedValidationMsg = "Login failed : Invalid Username or Password!";
+
+        Optional<User> optionalUser = userRepository.findByUsername(username);
+        if (optionalUser.isEmpty()) {
+            log.error("User with username: {}, does not exist.", username);
+            throw new SecurityException(failedValidationMsg);
+        }
+
+        User user = optionalUser.get();
+        if (!user.isActive()) {
+            log.error("User with username: {}, is currently not active.", username);
+            throw new SecurityException(failedValidationMsg);
+        }
+
+        if (!user.isVerified()) {
+            log.error("User with username: {}, is not verified.", username);
+            throw new SecurityException(failedValidationMsg);
+        }
+
+        log.debug("User Status validation complete for User: {}", user.getId());
     }
 
     public Optional<String> registerUser(SignUpRequest signUpRequest) {
@@ -143,9 +169,90 @@ public class UserService extends AbstractService<User> {
         confirmationTokenRepository.save(confirmToken);
 
         String emailText = emailSender.buildConfirmationEmail(user.getFirstName(), user.getUsername(), token);
-        emailSender.send(user.getContactDetail().getEmailAddress(), "Untu Credit Application - Account Verification", emailText);
+         emailSender.send(user.getContactDetail().getEmailAddress(), "Untu Credit Application Account Verification", emailText);
+        // emailSender.sendMail(user.getContactDetail().getEmailAddress(), "Untu Credit Application Account Verification", emailText);
 
         return Optional.of(createdUser.getId());
+    }
+
+    private String buildConfirmationEmail(String firstName, String username, String token) {
+        return "<div style=\"font-family:Helvetica,Arial,sans-serif;font-size:16px;margin:0;color:#0b0c0c\">\n" +
+                "\n" +
+                "<span style=\"display:none;font-size:1px;color:#fff;max-height:0\"></span>\n" +
+                "\n" +
+                "  <table role=\"presentation\" width=\"100%\" style=\"border-collapse:collapse;min-width:100%;width:100%!important\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\">\n" +
+                "    <tbody><tr>\n" +
+                "      <td width=\"100%\" height=\"53\" bgcolor=\"#0b0c0c\">\n" +
+                "        \n" +
+                "        <table role=\"presentation\" width=\"100%\" style=\"border-collapse:collapse;max-width:580px\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" align=\"center\">\n" +
+                "          <tbody><tr>\n" +
+                "            <td width=\"70\" bgcolor=\"#0b0c0c\" valign=\"middle\">\n" +
+                "                <table role=\"presentation\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse\">\n" +
+                "                  <tbody><tr>\n" +
+                "                    <td style=\"padding-left:10px\">\n" +
+                "                  \n" +
+                "                    </td>\n" +
+                "                    <td style=\"font-size:28px;line-height:1.315789474;Margin-top:4px;padding-left:10px\">\n" +
+                "                      <span style=\"font-family:Helvetica,Arial,sans-serif;font-weight:700;color:#ffffff;text-decoration:none;vertical-align:top;display:inline-block\">Confirm your email</span>\n" +
+                "                    </td>\n" +
+                "                  </tr>\n" +
+                "                </tbody></table>\n" +
+                "              </a>\n" +
+                "            </td>\n" +
+                "          </tr>\n" +
+                "        </tbody></table>\n" +
+                "        \n" +
+                "      </td>\n" +
+                "    </tr>\n" +
+                "  </tbody></table>\n" +
+                "  <table role=\"presentation\" class=\"m_-6186904992287805515content\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;max-width:580px;width:100%!important\" width=\"100%\">\n" +
+                "    <tbody><tr>\n" +
+                "      <td width=\"10\" height=\"10\" valign=\"middle\"></td>\n" +
+                "      <td>\n" +
+                "        \n" +
+                "                <table role=\"presentation\" width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse\">\n" +
+                "                  <tbody><tr>\n" +
+                "                    <td bgcolor=\"#1D70B8\" width=\"100%\" height=\"10\"></td>\n" +
+                "                  </tr>\n" +
+                "                </tbody></table>\n" +
+                "        \n" +
+                "      </td>\n" +
+                "      <td width=\"10\" valign=\"middle\" height=\"10\"></td>\n" +
+                "    </tr>\n" +
+                "  </tbody></table>\n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "  <table role=\"presentation\" class=\"m_-6186904992287805515content\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;max-width:580px;width:100%!important\" width=\"100%\">\n" +
+                "    <tbody><tr>\n" +
+                "      <td height=\"30\"><br></td>\n" +
+                "    </tr>\n" +
+                "    <tr>\n" +
+                "      <td width=\"10\" valign=\"middle\"><br></td>\n" +
+                "      <td style=\"font-family:Helvetica,Arial,sans-serif;font-size:19px;line-height:1.315789474;max-width:560px\">\n" +
+                "        \n" +
+                "            <p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\">Dear " + firstName + ",</p><p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\"> Thank you for registering on Untu Capital Credit Application. Please click on the below link to activate your account: " +
+                "               </p><blockquote style=\"Margin:0 0 20px 0;border-left:10px solid #b1b4b6;padding:15px 0 0.1px 15px;font-size:19px;line-height:25px\"><p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\"> <a href=\"http://localhost:7878/api/utg/auth/confirm_account?username=" + username + "&code=" + token + "\">Confirm Account</a> </p></blockquote>\n Link will expire in 30 minutes. <p>Cheers</p>\n<p>Untu Credit Application Team</p>" +
+                "        \n" +
+                "      </td>\n" +
+                "      <td width=\"10\" valign=\"middle\"><br></td>\n" +
+                "    </tr>\n" +
+                "    <tr>\n" +
+                "      <td height=\"30\"><br></td>\n" +
+                "    </tr>\n" +
+                "  </tbody></table><div class=\"yj6qo\"></div><div class=\"adL\">\n" +
+                "\n" +
+                "</div></div>";
+    }
+
+    private String createRegEmailText(String firstName, String lastName, String username, String code) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n Dear ").append(firstName).append(" ").append(lastName)
+                .append(", \n\n Follow the link below to verify your email address \n http://localhost:4200/authentication/account-confirm?username=")
+                .append(username)
+                .append("&code=").append(code)
+                .append("\n\n\n Regards \n Untu Credit Application Team.");
+        return sb.toString();
     }
 
     public boolean confirmUserAccount(String username, String verificationCode) {
