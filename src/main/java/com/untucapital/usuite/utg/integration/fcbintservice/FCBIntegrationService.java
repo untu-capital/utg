d9a2ed92ba662f68b7c775aba88fcd92baaf8a46
Nyasha Chirinda\Dell <nyasha.chirinda@untu-capital.com>
2021-12-10 11:48:49 +0200

    add fcb integration service

---
+++ src/main/java/com/untucapital/usuite/utg/integration/fcbintservice/FCBIntegrationService.java
@@ -0,0 +1,107 @@
+package com.untucapital.usuite.utg.integration.fcbintservice;
+
+import com.untucapital.usuite.utg.model.ClientLoan;
+import com.untucapital.usuite.utg.model.fcb.Report;
+import com.untucapital.usuite.utg.model.fcb.Response;
+import com.untucapital.usuite.utg.model.fcb.enums.*;
+import com.untucapital.usuite.utg.utils.DateTimeFormatterUtil;
+import com.untucapital.usuite.utg.utils.FormatterUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.untucapital.usuite.utg.config.FCBConfig.*;
+
+/**
+ * @author Chirinda Nyasha Dell - 7/12/2021
+ */
+
+@Service
+public class FCBIntegrationService {
+
+    private static final Logger log = LoggerFactory.getLogger(FCBIntegrationService.class);
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public Optional<Response> performSearch(ClientLoan clientLoan) {
+
+        MultiValueMap<String, Object> searchHeaders = createReqHeaders(clientLoan);
+
+        HttpEntity<Object> searchEntity = new HttpEntity<>(searchHeaders);
+
+        log.info("FCB Perform Search Request with Params - {}", FormatterUtil.toJson(searchHeaders));
+
+        // ResponseEntity<Response> responseEntity = restTemplate.exchange(PERFORM_SEARCH_URL, HttpMethod.POST, searchEntity, Response.class);
+
+        ResponseEntity<Object> responseEntity = restTemplate.postForEntity(PERFORM_SEARCH_URL, searchEntity, Object.class);
+
+        String jsonResponse = FormatterUtil.toJson(responseEntity.getBody());
+
+        log.info("FCB Perform Search Response - {}", jsonResponse);
+
+        Response checkResponse;
+        if (HttpStatus.OK.equals(responseEntity.getStatusCode())) {
+
+            checkResponse = FormatterUtil.toFCBResponse(jsonResponse);
+
+            // FCB is sending the Report inside the string property individual, converting it here
+            List<Report> reports = FormatterUtil.toFCBReport(checkResponse.getIndividual());
+
+            checkResponse.setReport(reports);
+
+            log.info("Client Credit Check Response - {}", FormatterUtil.toJson(checkResponse));
+
+            return Optional.of(checkResponse);
+
+        } else {
+            log.info("Credit Check Failed Response - {}", FormatterUtil.toJson(responseEntity.getBody()));
+        }
+
+        return Optional.empty();
+    }
+
+    private MultiValueMap<String, Object> createReqHeaders(ClientLoan clientLoan) {
+        MultiValueMap<String, Object> headers = new LinkedMultiValueMap<>();
+        headers.add("Content-Type", "application/json");
+
+        headers.set("email", USERNAME);
+        headers.set("password", PASSWORD);
+        headers.set("search_purpose", SearchPurpose.LOAN_RENEWAL_REPEAT_LOAN.code);
+        headers.set("names", clientLoan.getFirstname());
+        headers.set("surname", clientLoan.getLastname());
+        headers.set("gender", clientLoan.getGender().substring(0, 1));
+        headers.set("national_id", clientLoan.getId_number());
+        headers.set("nationality", "3");
+        headers.set("dob", DateTimeFormatterUtil.fcbDobFormatter(clientLoan.getDob()));
+        headers.set("married", MaritalStatus.valueOf(clientLoan.getMarital().toUpperCase()).code);
+        // headers.set("id", "");
+        headers.set("passport", "");
+        headers.set("drivers_license", "");
+        headers.set("streetno", clientLoan.getStreet_no());
+        headers.set("streetname", clientLoan.getStreet_name());
+        headers.set("suburb", clientLoan.getSurbub());
+        headers.set("building", "");
+        headers.set("city", clientLoan.getCity());
+        // headers.set("pbag", "");
+        headers.set("ind_email", "");
+        headers.set("mobile", clientLoan.getPhonenumber());
+        headers.set("telephone", "");
+        headers.set("employer", "");
+        headers.set("employer_industry", clientLoan.getIndustry().getCode());
+        headers.set("salary_band", Salary._0_TO_150.code);
+        headers.set("loan_purpose", LoanPurpose.BUSINESS_IMPROVEMENT_LOAN.code);
+        headers.set("loan_amount", clientLoan.getLoanAmount());
+        headers.set("occupation_class", OccupationClass.N_A.code);
+        headers.set("property_density", PropertyDensity.MEDIUM.code);
+        headers.set("property_status", PropertyOwnership.PARENTS.code);
+        return headers;
+    }
+}
