8b162dfddb81304eda13fb845cc4cc25b57b0d11
Kelvin Randa <kelvin.randa@untu-capital.com>
2022-01-03 16:53:00 +0200

    fcb columns

---
+++ src/main/java/com/untucapital/usuite/utg/model/fcb/Exposure.java
@@ -0,0 +1,11 @@
+package com.untucapital.usuite.utg.model.fcb;
+
+import com.untucapital.usuite.utg.model.AbstractEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "fcb_exposures")
+public class Exposure extends AbstractEntity {
+}
