5835ef02d1ec9abdfcd033005e4d7cfef1b83015
Kelvin Randa <kelvin.randa@untu-capital.com>
2022-01-27 08:38:34 +0200

    added file uploads method

--- src/main/java/com/untucapital/usuite/utg/controller/FileDownloadController.java
+++ src/main/java/com/untucapital/usuite/utg/controller/FileDownloadController.java
@@ -1,12 +1,16 @@
 package com.untucapital.usuite.utg.controller;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import com.untucapital.usuite.utg.model.ClientLoan;
 import com.untucapital.usuite.utg.model.DatabaseFile;
+import com.untucapital.usuite.utg.payload.Response;
 import com.untucapital.usuite.utg.repository.ClientRepository;
 import com.untucapital.usuite.utg.repository.DatabaseFileRepository;
 import com.untucapital.usuite.utg.service.DatabaseFileService;
+import org.hibernate.engine.jdbc.StreamUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
@@ -15,8 +19,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.io.IOException;
+import java.sql.Blob;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "credit_application")
@@ -44,9 +54,10 @@ public class FileDownloadController {
         return new ResponseEntity<List<DatabaseFile>>(databaseFileRepository.findByUserId(userId), HttpStatus.OK);
     }
 
-//    @GetMapping("/downloadFiless")
-//    public ResponseEntity<DatabaseFileService> getUploadFilesByFileId(@RequestParam String fileId) {
-//        return new ResponseEntity<DatabaseFileService>(fileStorageService.getFile(fileId),HttpStatus.OK);
-//    }
+    // Excel appraisal.. select using userid and description = appraisal
+    @GetMapping("/appraisal")
+    public ResponseEntity<List<DatabaseFile>> getUploadFilesByAppraisal(@RequestParam String userId, @RequestParam String fileDescription) {
+        return new ResponseEntity<List<DatabaseFile>>(databaseFileRepository.findByUserIdAndFileDescription(userId, fileDescription), HttpStatus.OK);
+    }
 
 }
