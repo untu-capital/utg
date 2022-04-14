0522ad41ceae9fed0fd102cfc211f3f533ec9ff3
UNTUCONNECT\tonderaim <tonderai.madende@untu-capital.com>
2022-04-14 09:02:10 +0200

    apprai

--- src/main/java/com/untucapital/usuite/utg/micro/qualitativeAssesment/service/OtherBusinessAndIncomeService.java
+++ src/main/java/com/untucapital/usuite/utg/micro/qualitativeAssesment/service/OtherBusinessAndIncomeService.java
@@ -5,8 +5,10 @@ import com.untucapital.usuite.utg.micro.qualitativeAssesment.repository.OtherBus
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
+@Transactional
 @Service
 public class OtherBusinessAndIncomeService {
     @Autowired
