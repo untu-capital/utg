5a1250b1e02b5d7c2e3ae81608401da35e3890e1
Kelvin Randa <kelvin.randa@untu-capital.com>
2022-05-05 16:17:55 +0200

    Client file upload

---
+++ src/main/java/com/untucapital/usuite/utg/repository/ClientFileUploadRepository.java
@@ -0,0 +1,11 @@
+package com.untucapital.usuite.utg.repository;
+
+import com.untucapital.usuite.utg.model.ClientFileUpload;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ClientFileUploadRepository extends JpaRepository<ClientFileUpload, String> {
+
+    List<ClientFileUpload> findClientFileUploadByUserId(String userId);
+}
