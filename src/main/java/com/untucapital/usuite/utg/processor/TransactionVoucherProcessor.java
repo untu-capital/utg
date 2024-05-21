b79bc14950406219435c31671a9d60cf00a244d4
tjchidanika <tjchidanika@gmail.com>
2024-05-21 15:02:54 +0200

    Updated Notifications When Transactions Are Processed

--- src/main/java/com/untucapital/usuite/utg/processor/TransactionVoucherProcessor.java
+++ src/main/java/com/untucapital/usuite/utg/processor/TransactionVoucherProcessor.java
@@ -22,7 +22,6 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
